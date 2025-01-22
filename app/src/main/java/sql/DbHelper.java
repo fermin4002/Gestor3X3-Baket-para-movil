642bf225b97ed072ba6699e3331b5303d5157d63
Fermin <fermin4002@gmail.com>
2025-01-22 22:31:12 +0100

    nombre bbdd

--- app/src/main/java/sql/DbHelper.java
+++ app/src/main/java/sql/DbHelper.java
@@ -45,7 +45,7 @@ public class DbHelper extends SQLiteOpenHelper {
 
 
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "Cat.db";
+    public static final String DATABASE_NAME = "NBA.db";
 
     public DbHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
